fbe7eaccf713cab805b09b996e9d08270aeabe44
Hanuman Sahay Jat <hanu9090@gmail.com>
2025-07-29 21:26:57 +0530

    updated go compile cmd

--- db/languages/active.rb
+++ db/languages/active.rb
@@ -384,7 +384,7 @@
     name: "Go (latest stable)",
     is_archived: false,
     source_file: "main.go",
-    compile_cmd: "GOCACHE=/tmp/.cache/go-build GOTMPDIR=/tmp/go-build /usr/local/bin/go build -ldflags=-s -ldflags=-w %s main.go",
+    compile_cmd: "mkdir -p /tmp/.cache/go-build /tmp/go-build && GOCACHE=/tmp/.cache/go-build GOTMPDIR=/tmp/go-build /usr/local/bin/go build -ldflags=-s -ldflags=-w %s main.go",
     run_cmd: "./main"
   },
   {
